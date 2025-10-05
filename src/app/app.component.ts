ec892645f6d1267a0a9efa8b0ece735c8abb5bb1
builtbypradeep <kpradeep29.pk@gmail.com>
2025-10-05 17:59:41 +0530

    Layout updated & null value handled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,31 @@ import { empty } from 'rxjs';
   selector: 'app-root',
   imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'TO-DO LIST';
 
-  task : string = "";
+  task: string = '';
 
   count = 0;
 
-  taskList:{id : number, task :string}[] = [];
+  taskList: { id: number; task: string }[] = [];
 
-  addTask(value:string){
-    
-    const index = this.count += 1;
-    this.taskList.push({id : index, task : this.task});
+  addTask(value: string) {
+    if (this.task != '') {
+      const index = (this.count += 1);
+      this.taskList.push({ id: index, task: this.task });
+    }
+    else{
+      alert("Task can't be empty");
+    }
+
     this.task = '';
-
   }
 
-  DeleteTask(id : number){
-    this.taskList = this.taskList.filter(task => task.id != id);
+  DeleteTask(id: number) {
+    this.taskList = this.taskList.filter((task) => task.id != id);
+    
   }
-
 }
