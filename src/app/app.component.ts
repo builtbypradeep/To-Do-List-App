73c345f2eeca80ca6e69c233f8409f0d0e2a6178
builtbypradeep <kpradeep29.pk@gmail.com>
2025-10-05 15:27:49 +0530

    fix

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { empty } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'TO-DO LIST';
+
+  task : string = "";
+
+  count = 0;
+
+  taskList:{id : number, task :string}[] = [];
+
+  addTask(value:string){
+    
+    const index = this.count += 1;
+    this.taskList.push({id : index, task : this.task});
+    this.task = '';
+
+  }
+
+  DeleteTask(id : number){
+    this.taskList = this.taskList.filter(task => task.id != id);
+  }
+
+}
